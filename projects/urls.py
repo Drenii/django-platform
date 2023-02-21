410c056ec9698daba3d8ece9f2de68c125e96e7f
Drenii <dreni107@hotmail.com>
2023-02-21 01:50:52 +0100

    All crud methods added

--- projects/urls.py
+++ projects/urls.py
@@ -6,6 +6,7 @@ urlpatterns=[
     path('project/<str:pk>/', views.project,name='project'),
     path('create-project/',views.createProject, name='create-project'),
     path('create-project/<str:pk>/', views.updateProject,name='update-project'),
+path('delete-project/<str:pk>/', views.deleteProject,name='delete-project'),
 
 
 ]
