6051676a2fd50dbe3a6effc8adc30d238d40b6f6
Drenii <dreni107@hotmail.com>
2023-03-01 12:12:06 +0100

    displaying data in frontend

--- dev_search/urls.py
+++ dev_search/urls.py
@@ -6,7 +6,7 @@ from django.conf.urls.static import static
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin', admin.site.urls),
     path('', include('projects.urls'))
 
 ]
